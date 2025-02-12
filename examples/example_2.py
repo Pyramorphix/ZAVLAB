808727962242a1e083fb3806d6e8bad35256c057
ArinaLuzgina <LusginaArinaA@gmail.com>
2025-02-12 22:57:13 +0300

    no more warnings

--- examples/example_2.py
+++ examples/example_2.py
@@ -50,14 +50,16 @@ b = 1.1 - 2 * k
 
 example_graph = Earl(verbose=False)
 
-example_graph.plot_graph(data_array=data_array, rows_cols=[2, 2],labels=labels, subplots_distribution=[0, 0, 0, 0, 1, 2, 2, 3],
+example_graph.plot_graph(data_array=data_array, rows_cols=[2, 2], subplots_distribution=[0, 0, 0, 0, 1, 2, 2, 3],
                          ls=['-'] * 5 + [''] + ['-'] * 2, logarithmic_scaling=[[0, [1, 1]]], line_width=[1] * 4, marker_size=3, marker_shape=["P"] * 6 + [""] * 2, axes_titles=[["some X title", "some Y title"], ["x", "y", "some cbar title"], [r"$\nu$, Гц", "U, Вт"]], axes_round_accuracy=[[0, ["%0.0f", "%0.0f"]], [1, ["%0.0f", "%0.0f"]], [2, ["%0.2f", "%0.0f"]], [3, ["%0.0f", "%0.2f"]]],
                          colormap=[[1, "Spectral"]], axes_scaling=[[1, "divide", [[-10, 10, 11], [-10, 10, 11]]], [2, "divide", [[0.88, 1.12, 4], [3, 27, 4]]], [3, "divide", [[-10, 10, 11],[-np.pi / 2 - 0.1, np.pi / 2 + 0.1, 11]]]], graph_types=["2D"] * 4 + ["3D"] + ["2D"] * 2)
 example_graph.draw_lines(start_point=[[1, 3], [0.88, 24.58], [-10, k * -10 + b]], end_point=[[1, 24.58], [1, 24.58], [2, 1.1]], text=[r"$\nu=31.3$Гц", "Q=24.5", "касательная к точке 2"], subplot_pos_line=[2, 2, 3], text_pos=[[False, False], [0.9, 25], [-6, -0.33]], text_rotation=[10, -90, -47])
-example_graph.show_plot()
-example_graph.save_plot(name="../examples/example.png")
-example_graph.save_config(name="../examples/new_config.json")
-example_graph.save_config_for_lines(name="../examples/new_config_for_lines.json")
-example_graph.change_config_file(file_path_name_to_conf="../examples/new_config.json")
+#example_graph.show_plot()
+example_graph.save_plot(name="./example.png")
+example_graph.save_config(name="./new_config.json")
+example_graph.save_config_for_lines(name="./new_config_for_lines.json")
+example_graph.change_config_file(file_path_name_to_conf="./new_config.json")
+example_graph.change_config_for_lines_file(name_of_config_file="./new_config_for_lines.json")
 example_graph.plot_graph(data_array=data_array)
+example_graph.draw_lines()
 example_graph.show_plot()
